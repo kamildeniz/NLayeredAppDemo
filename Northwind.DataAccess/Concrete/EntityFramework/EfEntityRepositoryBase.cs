e59eaa8e9b0ecafbb1a91532435d89d515baaf80
Kamil Deniz <kamil07deniz@gmail.com>
2023-02-20 15:07:34 +0300

    Add project files.

---
+++ Northwind.DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using Northwind.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework
+{
+    public interface EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+    {
+
+    }
+}
---
+++ Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,54 @@
+﻿using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Products.ToList(filter);
+            }
+        }
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Products.SingleOrDefault(filter);
+            }
+        }
+        public void Add(Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void Update(Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                // context.Products.AddOrUpdate(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
